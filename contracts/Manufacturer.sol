b1b3ead0b40ddf71baca96b1402eec13e8e3579d
LucStr <luca@mail.com>
2023-12-18 21:36:53 +0100

    add contracts

---
+++ contracts/Manufacturer.sol
@@ -0,0 +1,97 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Manufacturer is IERC20, Ownable {
+    uint256 private _totalSupply;
+
+    mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) private _allowances;
+
+    constructor(uint256 initialSupply) {
+        _totalSupply = initialSupply;
+        _balances[msg.sender] = initialSupply;
+    }
+
+    function totalSupply() external view override returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account)
+        external
+        view
+        override
+        returns (uint256)
+    {
+        return _balances[account];
+    }
+
+    function transfer(address, uint256) external pure override returns (bool) {
+        revert("Transfers are not allowed for this token.");
+    }
+
+    function allowance(address, address)
+        external
+        pure
+        override
+        returns (uint256)
+    {
+        return 0;
+    }
+
+    function approve(address, uint256) external pure override returns (bool) {
+        revert("Approvals are not allowed for this token.");
+    }
+
+    function transferFrom(
+        address,
+        address,
+        uint256
+    ) external pure override returns (bool) {
+        revert("Transfers are not allowed for this token.");
+    }
+
+    function mintToken(address account, uint256 amount) external onlyOwner {
+        require(account != address(0), "Invalid address");
+        require(amount > 0, "Amount must be greater than 0");
+        require(_totalSupply + amount >= _totalSupply, "Overflow");
+
+        _totalSupply += amount;
+        _balances[account] += amount;
+        emit Transfer(address(0), account, amount);
+    }
+
+    function transferToken(address account, uint256 amount)
+        external
+        onlyOwner
+    {
+        require(account != address(0), "Invalid address");
+        require(amount > 0, "Amount must be greater than 0");
+        require(_balances[msg.sender] >= amount, "Insufficient balance");
+
+        _balances[msg.sender] -= amount;
+        _balances[account] += amount;
+        emit Transfer(msg.sender, account, amount);
+    }
+
+    function retractToken(address account, uint256 amount) external onlyOwner {
+        require(account != address(0), "Invalid address");
+        require(amount > 0, "Amount must be greater than 0");
+        require(_balances[account] >= amount, "Insufficient balance");
+
+        _balances[account] -= amount;
+        _balances[msg.sender] += amount;
+        emit Transfer(account, msg.sender, amount);
+    }
+
+    function burnToken(uint256 amount) external onlyOwner {
+        require(_balances[msg.sender] >= amount, "Insufficient balance");
+        require(_totalSupply >= amount, "Total supply cannot be negative");
+
+        _balances[msg.sender] -= amount;
+        _totalSupply -= amount;
+        emit Transfer(msg.sender, address(0), amount);
+    }
+}
---
+++ contracts/ProductNFT.sol
@@ -0,0 +1,114 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+contract ProductNFT is ERC721, ERC721Burnable, Ownable {
+    struct Prpduct {
+        string brand;
+        string model;
+        string serialNumber;
+        string TransferLogs;
+    }
+
+    struct Transfer {
+        address station;
+        string name;
+    }
+
+    Transfer[] public TransferHistory;
+
+    mapping(uint256 => Product) public products;
+
+    IERC20 public tokenContract;
+
+    function checkTokenBalance(address userAddress)
+        public
+        view
+        returns (uint256)
+    {
+        return tokenContract.balanceOf(userAddress);
+    }
+
+    modifier onlyManufacturer() {
+        require(
+            checkTokenBalance(msg.sender) >= 2,
+            "Not authorized to mint");
+        _;
+    }
+
+    modifier onlyTransfer() {
+        require(
+            checkTokenBalance(msg.sender) >= 1,
+            "Not authorized to service"
+        );
+        _;
+    }
+
+    constructor(address _tokenContractAddress) ERC721("Product", "WTK") {
+        tokenContract = IERC20(_tokenContractAddress);
+    }
+
+    function mintProduct(
+        string memory brand,
+        string memory serialNumber
+    ) public onlyManufacturer returns (uint256) {
+        Product memory newProduct = Product(brand, model, serialNumber, "");
+        uint256 tokenId = uint256(
+            keccak256(abi.encode(brand, model, serialNumber))
+        );
+        protducts[tokenId] = newProduct;
+        _safeMint(msg.sender, tokenId);
+        appendLog(tokenId, "Heute", brand, "Hersteller hat Produkt erstellt.");
+        return tokenId;
+    }
+
+    function getProductDetails(uint256 tokenId)
+        public
+        view
+        returns (Product memory)
+    {
+        require(_exists(tokenId), "Token does not exist");
+        return products[tokenId];
+    }
+
+    function appendLog(uint256 tokenId, string memory date, string memory name, string memory logEntry)
+        public
+        onlyTransfer
+    {
+        require(_exists(tokenId), "NFT does not exist");
+        Product storage product = products[tokenId];
+
+        bytes memory jsonData = bytes(product.serviceLogs);
+
+        product.serviceLogs = string(abi.encodePacked(jsonData, "//"));
+
+        product.serviceLogs = string(
+            abi.encodePacked(product.serviceLogs, '{"Datum" : "', date, '", "Lieferant" : "', name, '", "Beschreibung" : "', logEntry, '"}')
+        );
+    }
+
+    // Override the safeTransferFrom function
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public override {
+        super.safeTransferFrom(from, to, tokenId);
+        appendLog(tokenId, "Heute", "Transfer", "Von 0x069086eE75c01D9A77138ED9F98D6F39Ac0233f1 nach 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4 transferiert.");
+    }
+
+    function getLogs(uint256 tokenId)
+        public
+        view
+        returns (string memory)
+    {
+        require(_exists(tokenId), "NFT does not exist");
+        return products[tokenId].serviceLogs;
+    }
+}
