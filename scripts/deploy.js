62a23677d5247eede4322c11245d3213a6a7eaea
LucStr <luca@mail.com>
2023-12-21 09:48:12 +0100

    cleanup deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,22 +6,23 @@ async function main() {
     console.log("Deploying contracts with the account:", deployer.address);
     
     const Manufacturer = await ethers.getContractFactory("Manufacturer");
-    const contract = await Manufacturer.deploy({ gasLimit: 6000000 });
-    await contract.deployed();
+    const manufacturer = await Manufacturer.deploy({ gasLimit: 6000000 });
+    await manufacturer.deployed();
 
-    console.log("Contract address:", contract.address);
+    //const token = await hre.ethers.deployContract("Manufacturer", 2, deployer.address);
+    console.log("Contract address:", manufacturer.address);
 
-    var debugText = await contract.mintToken(deployer.address, 20);
+    var debugText = await manufacturer.mintToken(deployer.address, 20);
     console.log(debugText);
 
     await new Promise(resolve => setTimeout(resolve, 20000));
 
-    var balance = await contract.balanceOf(deployer.address);
-    var total = await contract.totalSupply();
+    var balance = await manufacturer.balanceOf(deployer.address);
+    var total = await manufacturer.totalSupply();
     console.log("Balance: " + balance.toNumber() + "| Total Tokens: " + total.toNumber());
 
     const productToken = await ethers.getContractFactory("ProductNFT");
-    const productContract = await productToken.deploy(contract.address);
+    const productContract = await productToken.deploy(manufacturer.address, deployer.address);
     console.log("Product Contract address:", productContract.address);
   }
   
