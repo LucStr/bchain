17dc7f13ab13b0279245e9482525d9975edd60ea
LucStr <luca@mail.com>
2023-12-21 17:45:45 +0100

    proper naming of variables in deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,5 @@
 const {ethers} = require("hardhat");
+const path = require("path");
 
 async function main() {
     const [deployer] = await ethers.getSigners();
@@ -21,11 +22,56 @@ async function main() {
     var total = await manufacturer.totalSupply();
     console.log("Balance: " + balance.toNumber() + "| Total Tokens: " + total.toNumber());
 
-    const productToken = await ethers.getContractFactory("ProductNFT");
-    const productContract = await productToken.deploy(manufacturer.address, deployer.address);
-    console.log("Product Contract address:", productContract.address);
+    const ProductNFT = await ethers.getContractFactory("ProductNFT");
+    const productNFT = await ProductNFT.deploy(manufacturer.address);
+    console.log("Product Contract address:", productNFT.address);
+
+    const Token = await ethers.getContractFactory("Token");
+    const token = await Token.deploy();
+    await token.deployed();
+
+    console.log("Token address:", token.address);
+
+    saveFrontendFiles({
+      Manufacturer: manufacturer.address,
+      ProductNFT: productNFT.address,
+      Token: token.address,
+    })
   }
   
+function saveFrontendFiles(content) {
+  const fs = require("fs");
+  const contractsDir = path.join(__dirname, "..", "frontend", "src", "contracts");
+
+  if (!fs.existsSync(contractsDir)) {
+    fs.mkdirSync(contractsDir);
+  }
+
+  fs.writeFileSync(
+    path.join(contractsDir, "contract-address.json"),
+    JSON.stringify(content, undefined, 2)
+  );
+
+  const TokenArtifact = artifacts.readArtifactSync("Token");
+  const ManufacturerArtifact = artifacts.readArtifactSync("Manufacturer");
+  const ProductNFTArtifact = artifacts.readArtifactSync("ProductNFT");
+
+  fs.writeFileSync(
+    path.join(contractsDir, "Token.json"),
+    JSON.stringify(TokenArtifact, null, 2)
+  );
+
+  fs.writeFileSync(
+    path.join(contractsDir, "Manufacturer.json"),
+    JSON.stringify(ManufacturerArtifact, null, 2)
+  );
+
+  fs.writeFileSync(
+    path.join(contractsDir, "ProductNFT.json"),
+    JSON.stringify(ProductNFTArtifact, null, 2)
+  );
+}
+
 main()
     .then(() => process.exit(0))
     .catch((error) => {
